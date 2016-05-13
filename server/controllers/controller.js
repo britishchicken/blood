5ae3098c7383693d7a245e9d821f6cca63a62850
vinithasraja <snellhome1@gmail.com>
2016-05-13 15:00:48 -0700

    Add Buddy schema and update associations

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -90,7 +90,13 @@ Appointment.belongsTo(Donor);
 Hospital.hasMany(Appointment);
 Donor.hasMany(Appointment);
 
-
+let Buddy = sequelize.define('buddy', {
+ time: Sequelize.DATE,
+ found: Sequelize.BOOLEAN,
+});
+Buddy.belongsTo(Hospital);
+Buddy.belongsTo(Donor);
+Donor.hasMany(Buddy);
 
 sequelize.sync();
 
@@ -101,3 +107,4 @@ module.exports.Review = Review;
 module.exports.Schedule = Schedule;
 module.exports.Event = Event;
 module.exports.Appointment = Appointment;
+module.exports.Buddy = Buddy;
